7bb0b67e20c82e67b46ed8e1da6cdc2c6dbcd918
Yogeshpant992 <pantyogesh992@gmail.com>
2026-03-01 16:54:13 +0530

    if_else basic

---
+++ if_else.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    cin>>number;
+    if(number>0)
+    cout<<"Positive";
+    else if(number==0)
+    cout<<"zero";
+    else
+    cout<<"Negative";
+
+
+}
